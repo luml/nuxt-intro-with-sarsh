a312c3e6998f954389c62042149da8dd8cb936a1
luml <1520622848@qq.com>
2020-07-03 11:42:43 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flexrow">
+    <div class="unit" v-for="unit in manifest" :key="unit.item">
+      <child :item="unit.item" :url="unit.url"></child>
+    </div>
+  </div>
+</template>
+
+<script>
+import Child from "~/components/Child.vue";
+
+export default {
+  components: {
+    Child
+  },
+  data() {
+    return {
+      manifest: [
+        {
+          item: "backpack",
+          url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/backpack.jpg"
+        },
+        {
+          item: "tshirt",
+          url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/tshirt.jpg"
+        },
+        {
+          item: "sweatshirt",
+          url:
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/sweatshirt.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>

4378ad850ed8b5bf51120d225a612caf3aca8ac2
luml <1520622848@qq.com>
2020-07-03 15:30:20 +0800

    feat(part4: event handle example)

---
+++ part4/pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="container">
+    <div
+      id="app"
+      :style="{ backgroundColor: `hsl(${x}, 80%, 50%)` }"
+      @mousemove="xCoordinate"
+    >
+      <p>
+        <button @click="increment">+</button>
+        {{ counter }}
+        <button @click="decrement">-</button>
+      </p>
+      <p>Pixels across: {{ x }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      counter: 0,
+      x: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    },
+    xCoordinate(e) {
+      this.x = e.clientX;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+  padding: 15vmin;
+  font-size: 30px;
+  -webkit-transition: 0.2s background-color ease;
+  transition: 0.2s background-color ease;
+}
+</style>

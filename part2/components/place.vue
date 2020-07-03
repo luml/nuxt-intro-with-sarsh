bb2266ed0bb5c392496a00e73e1886683e039f48
luml <1520622848@qq.com>
2020-07-03 12:08:04 +0800

    feat(part2 for component for exercise)

---
+++ part2/components/place.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="place">
+    <img :src="location.img" width="235" height="300" alt="" />
+    <slot></slot>
+    <p>{{ location.desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["location"]
+};
+</script>
---
+++ part2/layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+body {
+  width: 100vw;
+  height: 100vh;
+  font-family: "NTR", sans-serif;
+  background: #eee;
+}
+
+h1 {
+  text-align: center;
+}
+
+.location-contain {
+  display: -webkit-box;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+}
+
+.place {
+  display: -webkit-box;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+  width: 280px;
+  height: 320px;
+  -webkit-box-pack: center;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+}
+.place h2 {
+  margin: 0;
+  text-align: center;
+}
+.place img {
+  margin: 10px;
+  align-self: center;
+}
+</style>

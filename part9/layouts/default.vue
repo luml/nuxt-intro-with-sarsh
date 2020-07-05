b6d1a9b49305f5120c21b459c745e639c206a112
luml <1520622848@qq.com>
2020-07-05 11:09:14 +0800

    feat(modal function for basic nuxt)

---
+++ part9/layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+body {
+  font-family: "Roboto Mono", serif;
+  display: -webkit-box;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+}
+
+#app {
+  text-align: center;
+  margin: 60px;
+  max-width: 370px;
+  display: -webkit-box;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+}
+
+button {
+  font-family: "Roboto Mono";
+  border: 2px solid black;
+  background: white;
+  padding: 10px 15px;
+  margin: 0 10px;
+  outline: 0;
+  width: 60%;
+  cursor: pointer;
+}
+
+h4 {
+  margin: 0 0 15px;
+}
+
+.modal {
+  background: cyan;
+  color: black;
+  padding: 20px;
+  width: 200px;
+  position: absolute;
+}
+</style>
---
+++ part9/pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="container">
+    <div id="app">
+      <h3>Let's triggrt this here modal!</h3>
+      <button @click="toggleShow">
+        <span v-if="isShowing">Hide child</span>
+        <span v-else>Show child</span>
+      </button>
+      <appChild v-id="isShowing" class="modal">
+        <button @click="toggleShow">Close</button>
+      </appChild>
+    </div>
+    <script type="text/x-template" id="childarea">
+      <div>
+      	<h2>Here I am!</h2>
+      <slot></slot>
+      </div>
+    </script>
+  </section>
+</template>
+
+<script>
+const Child = {
+  template: "#childarea"
+};
+export default {
+  data() {
+    return {
+      isShowing: false
+    };
+  },
+  methods: {
+    toggleShow() {
+      this.isShowing = !this.isShowing;
+    }
+  },
+  components: {
+    appChild: Child
+  }
+};
+</script>
+
+<style></style>

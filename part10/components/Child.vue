c3a110ff465bf97c068b47690f3ae5b0489ea479
luml <1520622848@qq.com>
2020-07-06 11:07:51 +0800

    feat(Vue transition modes doesn't work on Nuxt)

---
+++ part10/components/Child.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="childarea">
+    <div class="img-contain">
+      <div class="overlay">
+        <p class="overlay-text">I don't like this one</p>
+        <button @click="toggleShow">Replace!</button>
+      </div>
+      <transition name="flip" mode="out-in">
+        <slot v-if="!isShowing"></slot>
+        <img
+          v-else
+          src="https://images.unsplash.com/flagged/photo-1563248101-a975e9a18cc6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+          alt="replace pic"
+        />
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShowing: false
+    };
+  },
+  methods: {
+    toggleShow() {
+      this.isShowing = !this.isShowing;
+    }
+  }
+};
+</script>
---
+++ part10/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h3>Hover us</h3>
+    <main>
+      <appChild>
+        <img
+          src="https://images.unsplash.com/photo-1540057357560-ed0ed423cdcd?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+          alt="first pic"
+        />
+      </appChild>
+      <appChild>
+        <img
+          src="https://images.unsplash.com/photo-1520182205149-1e5e4e7329b4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+          alt="second pic"
+        />
+      </appChild>
+      <appChild>
+        <img
+          src="https://images.unsplash.com/photo-1501421018470-faf26f6b1bef?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+          alt="third pic"
+        />
+      </appChild>
+      <appChild>
+        <img
+          src="https://images.unsplash.com/photo-1521911528923-9c3838123490?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+          alt="fouth pic"
+        />
+      </appChild>
+    </main>
+  </div>
+</template>
+
+<script>
+import appChild from "./../components/Child.vue";
+export default {
+  components: { appChild }
+};
+</script>

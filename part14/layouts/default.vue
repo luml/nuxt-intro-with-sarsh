ad4d0ffcda1c24c6c93441623a63b1234f8a9928
luml <1520622848@qq.com>
2020-07-10 11:20:48 +0800

    feat(apply unuseful v-pre)

---
+++ part14/layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+body {
+  font-family: "Bitter", serif;
+}
+
+#app {
+  text-align: center;
+  padding: 70px;
+  max-width: 360px;
+  font-size: 16px;
+  margin: 0 auto;
+  display: table;
+  line-height: 2em;
+}
+
+label {
+  padding-right: 10px;
+}
+
+button {
+  border: none;
+  background: orangered;
+  color: white;
+  padding: 0.5em 1em;
+  border-radius: 3px;
+}
+</style>
---
+++ part14/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <div id="app">
+      <h3>What is your favorite kind of taco?</h3>
+      <p><input v-model="tacos" /></p>
+      <p v-bind="tacos">{{ tacos }}</p>
+      <span v-pre
+        >This is good if I need to show the mustache view of {{ tacos }}</span
+      >
+      <pre>{{ $data }}</pre>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      tacos: "I like Al Pastor tacos",
+      documents: "It's good to read, start it with us",
+      tips: "<code>heya</code>"
+    };
+  }
+};
+</script>

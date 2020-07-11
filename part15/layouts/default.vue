37de7c00586220e373daebf3d31dee755fa81f68
luml <1520622848@qq.com>
2020-07-11 19:38:45 +0800

    feat(chekbox example using v-model)

---
+++ part15/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+body {
+  font-family: "Bitter", serif;
+  background: lightYellow;
+}
+
+#app {
+  text-align: center;
+  padding: 70px;
+  max-width: 360px;
+  font-size: 16px;
+  margin: 0 auto;
+  display: table;
+  line-height: 2em;
+}
+
+label {
+  padding-right: 10px;
+}
+</style>
---
+++ part15/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="container">
+    <div id="app">
+      <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+      <label for="john">John</label>
+      <input type="checkbox" id="paul" value="Paul" v-model="checkedNames" />
+      <label for="paul"> Paul</label>
+      <input
+        type="checkbox"
+        id="george"
+        value="George"
+        v-model="checkedNames"
+      />
+      <label for="george"> George</label>
+      <input type="checkbox" id="ringo" value="Ringo" v-model="checkedNames" />
+      <label for="ringo"> Ringo</label>
+
+      <br />
+      <span>Checked names: {{ checkedNames }}</span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      checkedNames: []
+    };
+  }
+};
+</script>

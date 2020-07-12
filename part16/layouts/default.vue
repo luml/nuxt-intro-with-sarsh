6e9beca39c059421a85eafda50dd7402682dcb5d
luml <1520622848@qq.com>
2020-07-12 13:08:38 +0800

    feat(time teller)

---
+++ part16/layouts/default.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+body {
+  font-family: "Myriad Pro", Myriad, "Liberation Sans", "Nimbus Sans L",
+    "Helvetica Neue", Helvetica, Arial, sans-serif !important;
+  background: #f3f8fc;
+  /* Old browsers */
+  /* FF3.6-15 */
+  /* Chrome10-25,Safari5.1-6 */
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(#f3f8fc),
+    to(#efe6f9)
+  );
+  background: linear-gradient(to bottom, #f3f8fc 0%, #efe6f9 100%);
+  /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f3f8fc', endColorstr='#efe6f9',GradientType=0 );
+  /* IE6-9 */
+  width: 100vw;
+  height: 100vh;
+  min-height: 700px;
+}
+
+#app {
+  color: white;
+  max-width: 300px;
+  padding: 60px;
+  background: #2b323a;
+  /* Old browsers */
+  /* FF3.6-15 */
+  /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(135deg, #2b323a 0%, #1d0c1f 100%);
+  /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2b323a', endColorstr='#1d0c1f',GradientType=1 );
+  /* IE6-9 fallback on horizontal gradient */
+  margin: 30px auto;
+  display: table;
+  box-shadow: 16px 16px 32px rgba(30, 15, 33, 0.5);
+}
+
+select,
+option {
+  font-family: "Myriad Pro", Myriad, "Liberation Sans", "Nimbus Sans L",
+    "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+select {
+  background: transparent;
+  color: #b1afb8;
+  font-size: 18px;
+  margin: -23px auto;
+  border: 1px solid #4f4d53;
+  display: table;
+  line-height: 20px;
+  text-align: center;
+  position: relative;
+  z-index: 3000;
+}
+
+.dial {
+  width: 100%;
+  opacity: 0.85;
+}
+
+.tree-front-group {
+  opacity: 0.5;
+  -webkit-transform: translateX(-5px);
+  transform: translateX(-5px);
+}
+
+#moonstars {
+  opacity: 0;
+}
+
+svg text {
+  font-family: "Myriad Pro", Myriad, "Liberation Sans", "Nimbus Sans L",
+    "Helvetica Neue", Helvetica, Arial, sans-serif !important;
+}
+
+@media (max-width: 600px) {
+  #app {
+    max-width: 200px;
+    margin: 10px auto;
+  }
+
+  body {
+    min-height: 500px;
+  }
+}
+#app:not(:root:root) {
+  padding: 30px;
+}
+</style>
